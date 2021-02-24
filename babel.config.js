7752106009a5b0091c42f35539602d9593dd0d8e
Joy <hyewon3938@naver.com>
2021-02-24 15:56:11 +0900

    [#1] chore : 웹팩 바벨 환경설정
    - 웹팩 config 개발모드랑 제품모드 나눠서 설정
    - 바벨 config 설정

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { useBuiltIns: 'usage', targets: { browsers: ['>1%'] }, corejs: 3 }],
+    '@babel/preset-react'
+  ],
+  plugins: ['@babel/proposal-class-properties', '@babel/syntax-dynamic-import', '@babel/plugin-transform-runtime']
+};
