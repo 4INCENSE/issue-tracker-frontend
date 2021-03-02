ca8ac5cd6c64e6c4cda1b9ab6cf569da4adf4eea
Joy <hyewon3938@naver.com>
2021-03-03 02:49:07 +0900

    [#4] feat : FilterButton 팝업메뉴 UI 구현
    - FilterButton 팝업메뉴 UI 구현
    - 사용자 정보가 있는 Author과 Assignee, 그리고 labe과 milestone의 UI를 각각 구현하였다.
    - 스타일 객체를 추가하였다.
    - checked.svg 아이콘 추가

--- src/components/Main/IssueList/ListHead/FilterButton/FilterButton.jsx
+++ src/components/Main/IssueList/ListHead/FilterButton/FilterButton.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import arrow from '@/image/icon/arrow-point-to-down.svg';
+import checked from '@/image/icon/checked.svg';
+import profile from '@/image/profile.jpg';
 
 const FilterButton = ({ title }) => {
   return (
     <Wrap>
       {title}
       <img src={arrow} />
+      <ListPopupWrap>
+        <ListPopupMenu first>Filter by label</ListPopupMenu>
+        <ListPopupMenu>
+          <UserListContent>
+            <CheckedIcon>
+              <img src={checked} />
+            </CheckedIcon>
+            <img src={profile} /> hyewon3938 <span>Joy</span>
+          </UserListContent>
+        </ListPopupMenu>
+        <ListPopupMenu>
+          <LabelContent>
+            <CheckedIcon>
+              <img src={checked} />
+            </CheckedIcon>
+            <LabelColor />
+            <LabelDescriptionWrap>
+              feature
+              <span>New feature or request</span>
+            </LabelDescriptionWrap>
+          </LabelContent>
+        </ListPopupMenu>
+        <ListPopupMenu>
+          <CheckedIcon>
+            <img src={checked} />
+          </CheckedIcon>
+          <MilestoneContent>스프린트2</MilestoneContent>
+        </ListPopupMenu>
+      </ListPopupWrap>
     </Wrap>
   );
 };
@@ -15,11 +46,109 @@ const FilterButton = ({ title }) => {
 export default FilterButton;
 
 const Wrap = styled.div`
+  position: relative;
   display: flex;
   cursor: pointer;
   margin: auto 15px;
   img {
     margin: 0 0 0 5px;
     width: 12px;
+  }
+`;
+
+const ListPopupWrap = styled.div`
+  position: absolute;
+  top: 25px;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  min-width: 210px;
+  border: 1px solid ${({ theme }) => theme.Main.filterButton.border};
+  border-radius: 5px;
+  background: white;
+`;
+
+const ListPopupMenu = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  min-height: 30px;
+  border-top: 1px solid ${({ theme }) => theme.Main.filterButton.border};
+  padding: 5px 10px 5px 10px;
+  cursor: pointer;
+  &:hover {
+    background: ${({ theme }) => theme.Main.filter.buttonBackground};
+  }
+  ${(props) => {
+    if (props.first) {
+      return css`
+        border-top: none;
+        background: ${({ theme }) => theme.Main.filter.buttonBackground};
+        font-weight: bold;
+        &:hover {
+          background: ${({ theme }) => theme.Main.filter.buttonBackground};
+        }
+      `;
+    }
+  }};
+`;
+
+const UserListContent = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: ${({ theme }) => theme.fontSize.small};
+  font-weight: bold;
+  span {
+    color: gray;
+    font-weight: normal;
+    font-size: ${({ theme }) => theme.fontSize.small};
+    margin: 8px 5px 8px 5px;
+  }
+  img {
+    width: 23px;
+    height: 23px;
+    border-radius: 23px;
+    margin: 0 5px 0 0;
+  }
+`;
+
+const LabelContent = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin: 7px 5px;
+`;
+
+const LabelColor = styled.div`
+  width: 15px;
+  height: 15px;
+  border-radius: 5px;
+  background: blue;
+  margin: 0 5px 0 0;
+`;
+
+const LabelDescriptionWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: ${({ theme }) => theme.fontSize.small};
+  span {
+    color: gray;
+    font-size: ${({ theme }) => theme.fontSize.small};
+    margin: 5px 0 0 0;
+  }
+`;
+
+const MilestoneContent = styled.div`
+  font-weight: bold;
+  font-size: ${({ theme }) => theme.fontSize.small};
+  margin: 5px;
+`;
+
+const CheckedIcon = styled.div`
+  display: flex;
+  img {
+    width: 13px;
+    margin: 0 7px 0 0;
+    opacity: 0;
   }
 `;
