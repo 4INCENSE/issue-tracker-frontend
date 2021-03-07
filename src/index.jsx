0eebfc990524c0d39557214019a3f065694ed5a8
Joy <hyewon3938@naver.com>
2021-03-08 03:27:35 +0900

    [#3] feat : 체크박스 체크 리스트 관리를 위한 redux reducer 설정
    - index 파일에 rootReducer 설정, 및 checkboxReducer 등록
    - 리덕스 logger import

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,27 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import { checkboxReducer } from '@/redux/Main/reducers/checkboxReducer';
+
 const rootElement = document.getElementById('root');
-import(/*webpackChunkName: 'App' */ '@/App').then(({ default: App }) => ReactDOM.render(<App />, rootElement));
+
+const rootReducer = combineReducers({
+  checkboxReducer
+});
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk, logger)));
+
+import(/*webpackChunkName: 'App' */ '@/App').then(({ default: App }) =>
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  )
+);
